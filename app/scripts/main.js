d4b7d22fba9ffd0dfd0b9724fa9afaefb390a3e7
galvanize <galvanize@gPOa7af7215.local>
2018-03-21 10:05:29 -0600

    created an array of numbers. wrote function to find sum

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,3 +10,15 @@ const objects = [
         age: 89
     }
 ]
+
+const numbers = [1, 3, 5, 65, 78]
+
+function findSum(numbers) {
+    let sum = 0
+    for(let i = 0; i >= numbers.length; i++) {
+        sum += numbers[i]
+    }
+    return sum
+}
+
+findSum()
