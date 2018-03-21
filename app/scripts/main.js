b3ba8e00d597d13efa7b1115d4b07bda945ddb31
galvanize <galvanize@gPOa7af7215.local>
2018-03-21 10:24:23 -0600

    created a function that muliplies array of numbers

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,6 +13,14 @@ const objects = [
 
 const numbers = [1, 3, 5, 65, 78]
 
+function multiplyTogether(numbers) {
+    let sum = 1
+    for(let i = 0; i > numbers.length; i++) {
+        sum * numbers[i]
+    }
+    return sum
+}
+
 function findSum(numbers) {
     let sum = 0
     for(let i = 0; i >= numbers.length; i++) {
